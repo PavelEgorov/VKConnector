786639e4662f949a1aff1d42e58a8cea802dfecc
PavelEgorov <jonnycomein@gmail.com>
2020-03-31 14:24:40 +0300

    Убрал лишнее.

--- app/src/main/java/com/egorovsoft/vkconnector/mvp/presenter/fragment/AttachmentsPresenter.kt
+++ app/src/main/java/com/egorovsoft/vkconnector/mvp/presenter/fragment/AttachmentsPresenter.kt
@@ -2,8 +2,6 @@ package com.egorovsoft.vkconnector.mvp.presenter.fragment
 
 import com.egorovsoft.vkconnector.mvp.model.attachments.ItemAttachments
 import com.egorovsoft.vkconnector.mvp.view.fragment.attachments.AttachmentsView
-import com.egorovsoft.vkconnector.rx.EventBass
-import com.egorovsoft.vkconnector.rx.EventString
 import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -13,49 +11,23 @@ import moxy.MvpPresenter
 @InjectViewState
 class AttachmentsPresenter(val mainThread: Scheduler) : MvpPresenter<AttachmentsView>() {
     var item : ItemAttachments? = null
-    val bass = EventBass.getInstance().event
-    var disposable : Disposable? = null
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         viewState.init()
-
-        /// Пример использования, обновление форм
-
-        /// Присоединяюсь к автобусу
-        //disposable = EventBass.getInstance().observer.connect()
-
-        /// подписываюсь
-        disposable = bass
-            /// Странно когда вызываю подписку в разных потоках то не обновляется. Возможно дело в потоке. Не успевает получить значения
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(mainThread)
-            .filter {
-                it.className == this::class.toString()
-            }
-            .doOnNext{
-                if (it.funName == "setTitle"){
-                    viewState.setTitle(it.result)
-                }
-                if (it.funName == "setDescription"){
-                    viewState.setDescription(it.result)
-                }
-            }
-            .subscribe()
 
         updateFragment()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        disposable!!.dispose()
     }
 
     fun updateFragment() {
         item?.let {
             /// Отправляю данные на изменение
-            bass.onNext(EventString(this::class.toString(), "setTitle", it.title));
-            bass.onNext(EventString(this::class.toString(), "setDescription", it.description));
+            viewState.setTitle(it.title)
+            viewState.setDescription(it.description)
         }
     }
 }
